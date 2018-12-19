6847b7d10c6ae02df8ca197679ad1ecee83414c8
Victorio Matteucci <victorio.matteucci@gmail.com>
2018-12-19 12:24:00 -0300

    feat: add model
    add word class to model

---
+++ lib/model/word.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Word {
+  final int id;
+  final String value;
+  final Color color;
+
+  Word({@required this.id, @required this.value, @required this.color});
+}
